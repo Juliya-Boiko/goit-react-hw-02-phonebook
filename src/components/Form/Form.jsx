53d4ac4e48b42c167dff8af67e98fbe1a7102061
Juliya <BoikoJuliya2707@gmail.com>
2022-07-02 14:07:39 +0300

    formik+yup

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,10 +3,10 @@ import { nanoid } from 'nanoid';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
-const schema = yup.object().shape({
-  name: yup.string().length(3).required(),
-  number: yup.number().positive().integer().required(),
-});
+const mySchema = yup.object().shape({
+    name: yup.string().min(2).required(),
+    number: yup.string().length(7).required(),
+  });
 
 export class MyForm extends Component {
     state = {
@@ -14,64 +14,57 @@ export class MyForm extends Component {
         number: '',
     }
 
-    resetForm = () => {
-      this.setState({
-          name: '',
-          number: '',
-      })
+    normalizedNumber = (str) => {
+        const normalizedNumber = str[0] + str[1] + str[2] + '-' + str[3] + str[4] + '-' + str[5] + str[6];
+        return normalizedNumber;
     }
 
-    handlerInput = (evt) => {
-        this.setState({
-        [evt.target.name]: evt.target.value,
-        })
+    normalizedName = (str) => {
+        const normalizedName = str.split(' ').map((item) => item[0].toUpperCase() + item.slice(1)).join(' ');
+        return normalizedName;
     }
 
-    hadleSubmit = (evt) => {
-        evt.preventDefault();
+    hadleSubmit = (values, { resetForm }) => {
         const newName = {
             id: nanoid(),
-            name: evt.target.elements.name.value,
-            number: evt.target.elements.number.value,
+            name: this.normalizedName(values.name),
+            number: this.normalizedNumber(values.number),
         };
         this.props.onSubmit(newName);
-        this.resetForm();
+        resetForm();
     }
 
     render() {
         return <Formik
             initialValues={this.state}
-            validationSchema={schema}
+            validationSchema={mySchema}
+            onSubmit={this.hadleSubmit}
             >
-            <Form onSubmit={this.hadleSubmit}>
-            <label>
-                Name:
-                <Field
-                    type="text"
-                    name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    // required
-                    onChange={this.handlerInput}
-                    value={this.state.name}
-                    />
-                <ErrorMessage name="name" />
-            </label>
-            <label>
-                Number:
-                <Field
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                // required
-                onChange={this.handlerInput}
-                value={this.state.number}
-                    />
-                <ErrorMessage name="number" />
-            </label>
-            <button type="submit">Add contact</button>
-            </Form>
+            {props => (
+                <Form>
+                    <label>
+                        Name:
+                        <Field
+                            type="text"
+                            name="name"
+                            onChange={props.handleChange}
+                            value={props.values.name}
+                        />
+                        <ErrorMessage name="name" />
+                    </label>
+                    <label>
+                        Number:
+                        <Field
+                            type="tel"
+                            name="number"
+                            onChange={props.handleChange}
+                            value={props.values.number}
+                        />
+                        <ErrorMessage name="number" />
+                    </label>
+                    <button type="submit">Add contact</button>
+                </Form>
+            )}
         </Formik>
     }
 }
